f418f57d910e12f36bfd12f99ff5df80a2cfcb1d
SoundlyGifted <SoundlyGifted@DESKTOP-V2KI0GB>
2023-02-01 17:54:47 +0300

    Renamed the application packages

---
+++ BudgetPlanning-ejb/src/main/java/com/ejb/mainscreen/PlannedAccountsValuesSQLLocal.java
@@ -0,0 +1,65 @@
+
+package com.ejb.mainscreen;
+
+import java.sql.Connection;
+import java.util.Map;
+import java.util.TreeMap;
+import jakarta.ejb.Local;
+
+/**
+ * EJB PlannedAccountsValuesSQL Local interface contains methods to perform 
+ * operations on planned and calculated parameters data of the Accounts in the 
+ * database.
+ */
+@Local
+public interface PlannedAccountsValuesSQLLocal {
+
+    /**
+     * Updates Account's planned parameter data in the database.
+     * 
+     * @param connection database Connection.
+     * @param accountId database Account ID.
+     * @param paramName name of the planned parameter of the Account.
+     * @param updatedValues new values of the planned parameter mapped to the 
+     * planning Periods dates.
+     * @return "true" in case of success of the operation and "false" otherwise.
+     */
+    public boolean executeUpdate(Connection connection, String accountId,
+            String paramName, Map<String, String> updatedValues);
+
+    /**
+     * Selects and returns planned parameter values of a given Account mapped 
+     * to the planning Periods dates from database.
+     * 
+     * @param connection database Connection.
+     * @param id database Account ID.
+     * @param paramName name of the planned parameter of the Account.
+     * @return planned parameter values of a given Account mapped to the 
+     * planning Periods dates from database.
+     */
+    public TreeMap<String, Double> selectPlannedAccountsValuesById(Connection 
+            connection, Integer id, String paramName);
+    
+    /**
+     * Updates planned and calculated parameters of all the Accounts in the 
+     * database based on values from Account calculation class objects 
+     * contained in the list that is returned by 
+     * {@link ejb.calculation.AccountsHandlerLocal#getEntityAccountList()}
+     * 
+     * @param connection database Connection
+     * @param inputPlanningPeriodsFrequency planning Periods Frequency.
+     * @return "true" in case of success of the operation and "false" otherwise.
+     */
+    public boolean executeUpdateAll(Connection connection,
+            String inputPlanningPeriodsFrequency);
+    
+    /**
+     * Sets current Period in the database table that contains Accounts plan.
+     * 
+     * @param connection database Connection
+     * @param date the date of the new current planning Period in ISO8601 
+     * format.
+     * @return "true" in case of success of the operation and "false" otherwise.
+     */
+    public boolean setCurrentPeriodDate(Connection connection, String date);
+}
